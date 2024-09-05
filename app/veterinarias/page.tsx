c001bf3aa8ce5263f88526475c7637c7d768ae6f
neythan <neeycandia1001@gmail.com>
2024-09-05 18:22:44 -0300

    agregando la card

--- app/veterinarias/page.tsx
+++ app/veterinarias/page.tsx
@@ -2,7 +2,10 @@ import { Box, Container } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Image from "next/image";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
 
 export default function page() {
   return (
@@ -15,14 +18,39 @@ export default function page() {
             fill
           />
         </Box>
-        <Typography variant="h3" gutterBottom>
-          Ola gigante
-        </Typography>
-
-        <Typography variant="h4" gutterBottom>
-          1 1 1
-        </Typography>
+        <Box sx={{ minWidth: 275 }}>
+      
+          <Card variant="outlined">{card}</Card>
+        </Box>
       </Stack>
     </Container>
   );
 }
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+const card = (
+  <Container>
+    <CardContent>
+      <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+        Word of the Day
+      </Typography>
+      <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </Container>
+);
