f5079db371e20625e300cfe1229ae6cb0fbff640
neythan <neeycandia1001@gmail.com>
2024-11-21 15:43:12 -0300

    Agregamos los datos al prisma

--- db/registrar-primeras-veterinarias.mjs
+++ db/registrar-primeras-veterinarias.mjs
@@ -7,43 +7,32 @@ import { PrismaClient } from "@prisma/client";
       await db.veterinaria.createMany({
         data: [
           {
-            name: "beto",
             email: "vetspet@",
             numero: "01149034976",
             nombreDelLocal: "Veterinaria Pet Mundo",
             ubicacion: "Rojas 62, C1405AAB Cdad. Autónoma de Buenos Aires",
-            longitud: "-58.44190336062568",
-            latitud: "-34.61954373480866",
+            longitud: -58.44190336062568,
+            latitud: -34.61954373480866,
           },
-
-          //Veterinaria Sanitas
-          //
-          //numero:
-
           {
-            name: "beto",
             email: "sanitas@",
             numero: "01144323889",
             nombreDelLocal: "Veterinaria Sanitas",
             ubicacion:
               "Víctor Martínez 120, C1406 Cdad. Autónoma de Buenos Aires",
-            longitud: "-58.44477141829615",
-            latitud: "-34.62379086909921,",
-          },
-          {
-            name: "beto",
-            email: "",
-            numero: "",
-            nombreDelLocal: "vets cats",
-            ubicacion: "",
-            longitud: "",
-            latitud: "",
+            longitud: -58.44477141829615,
+            latitud: -34.62379086909921,
           },
         ],
       });
-    } catch (error) {}
+    } catch (error) {
+      console.log("no pude crear las veterinarias");
+      console.table(error);
+    } finally {
+    }
   } catch (error) {
     console.log("no me pude conectar");
+  } finally {
   }
 })();
 
