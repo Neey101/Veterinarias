bda6406b4122a7b739438c2bca3e8377446e0ba8
neythan <neeycandia1001@gmail.com>
2024-12-12 19:49:01 -0300

    tarjetas automaticas finalizadas

--- app/mapeo/components/GaleriaDeVeterinarias.tsx
+++ app/mapeo/components/GaleriaDeVeterinarias.tsx
@@ -1,16 +1,29 @@
 import { Divider, Stack } from "@mui/material";
 import React from "react";
 import TarjetaVeterinaria from "./TarjetaVeterinaria";
+import db from "@/db";
 
-const GaleriaDeVeterinarias = () => {
+const GaleriaDeVeterinarias = async () => {
+  const veterinarias = await db.veterinaria.findMany({});
   return (
     <Stack
       direction={"row"}
       divider={<Divider orientation="vertical" flexItem />}
       spacing={2}
     >
-      <TarjetaVeterinaria />
-      <TarjetaVeterinaria />
+      {veterinarias.map((veterinaria) => {
+        return (
+          <TarjetaVeterinaria
+            nombre={veterinaria.nombreDelLocal}
+            email={veterinaria.email}
+            numero={veterinaria.numero}
+            direccion={veterinaria.ubicacion}
+            foto={"https://picsum.photos/200/300?grayscale"}
+            latitud={veterinaria.latitud}
+            longitud={veterinaria.longitud}
+          />
+        );
+      })}
     </Stack>
   );
 };
